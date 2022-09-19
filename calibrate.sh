361d40e6e4204bf3c4a3a3718644d90884a9755b
Arthur Mochiuti Braghetto <arthurmb@gmail.com>
2022-09-18 21:04:22 -0300

    just comments

--- calibrate.sh
+++ calibrate.sh
@@ -7,6 +7,7 @@ SERIAL=$(lsusb -d 0bda: -v |grep iSerial |awk '{print $3}')
 green='\033[0;32m'
 clear='\033[0m'
 
+# initial error and best channel to run kal
 case $SERIAL in
 	00000001)
 		IERR=74
@@ -23,21 +24,28 @@ case $SERIAL in
 	;;
 esac
 
+# stop sdr services
 sudo systemctl stop spyserver.service
 sudo systemctl stop rtltcp.service
 sudo systemctl stop sdrpp.service
 
+# float ppm value
 PPM=$(/usr/local/bin/kal -g $GAIN -b $BAND -c $CHAN -e $IERR |grep "average absolute error:" |awk '{print $4}')
+# round ppm to integer
 PPMI=$(echo $PPM |awk '{print int($1+0.5)}')
+# multiply to ppb
 PPB="${PPM//.}"
+# print
 echo -e "${green}PPMi: $PPMI${clear}"
 echo -e "${green}PPM: $PPM${clear}"
 echo -e "${green}PPB: $PPB${clear}"
 
+# set ppm/ppb in configs
 /usr/bin/sed -i -e "/frequency_correction_ppb =/ s/= .*/= $PPB/" /etc/spyserver.config
 /usr/bin/sed -i -e "s/-P [0-9]\+/-P $PPMI/" /etc/systemd/system/rtltcp.service
 /usr/bin/sed -i -e "s/\"ppm\": [0-9]\+,/\"ppm\": $PPMI,/" ~/.config/sdrpp/rtl_tcp_config.json
 
+# start sdr services again
 sudo systemctl daemon-reload
 
 if [ "$(sudo systemctl is-enabled spyserver.service)" = "enabled" ]
@@ -55,5 +63,6 @@ then
 sudo systemctl start sdrpp.service
 fi
 
+# done
 exit 0
 
